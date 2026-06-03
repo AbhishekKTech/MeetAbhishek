d87f85451d742f0729168de2aa2ff0f52e5911a6
Abhishek kumar Sharma <abhishek859573@gmail.com>
2026-06-03 23:49:34 +0530

    Clean up experience data in data.ts
    Removed commented-out experience data and exported an empty experience array.

--- lib/data.ts
+++ lib/data.ts
@@ -224,20 +224,8 @@ export const skills = [
   }
 ]
 
-// export const experience = [
-//   {
-//     "title": "Website Developer Intern",
-//     "company": "Live Tech Service",
-//     "location": "Greater Noida, India",
-//     "startYear": "2023",
-//     "endYear": "2024",
-//     "isOngoing": false,
-//     "description": [
-//       "Built responsive WordPress & Shopify websites and handled ongoing maintenance for continuous availability.",
-//       "Executed hands-on SEO strategies to boost website visibility and drive key organizational growth."
-//     ]
-//   }
-// ]
+export const experience = [
+]
 
 export const education = [
   {
